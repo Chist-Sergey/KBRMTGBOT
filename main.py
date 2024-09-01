4856e7c07f1d8b56edf9e8317b561a752d19b7e7
Chist-Sergey <sergeychistov4088@gmail.com>
2024-09-02 00:56:43 +0700

    Added a 'create_rkb' function. Rewired the 'start' command to callback the 'create_rkb' function. Added a 'delete me' pattern-command. Rewired the 'delete me' command to callback the 'main' function.

--- main.py
+++ main.py
@@ -17,10 +17,26 @@ async def main(
     )
 
 
+async def create_rkb(
+    update: telegram.Update,
+    context: telegram.ext.ContextTypes.DEFAULT_TYPE
+) -> None:
+    """Removes any reply keyboard from a group chat."""
+    await context.bot.send_message(
+        text='Created.',
+        chat_id=update.effective_chat.id,
+        reply_markup=telegram.ReplyKeyboardMarkup(
+            [['delete me']]
+        ),
+    )
+
+
 if __name__ == '__main__':
     bot = telegram.ext.ApplicationBuilder(
     ).token(os.getenv('TOKEN')).build()
 
-    bot.add_handler(telegram.ext.CommandHandler('start', main))
+    bot.add_handler(telegram.ext.CommandHandler('start', create_rkb))
+    bot.add_handler(telegram.ext.MessageHandler(
+        telegram.ext.filters.Regex('delete me'), main))
 
     bot.run_polling(poll_interval=5)
